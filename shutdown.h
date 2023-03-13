8b7715417fafe1238550bdb292cb3bfab87a2c92
Travis <Toooo123@live.ca>
2023-03-13 14:34:07 -0700

    Clean up shutdown manager
    Leave inits and thread starting in main
    Let shutdown variable be changed locally

--- shutdown.h
+++ shutdown.h
@@ -3,19 +3,10 @@
 #ifndef _SHUTDOWN_H_
 #define _SHUTDOWN_H_
 
-#include <stdbool.h>
-
-// Initialize/cleanup the module's data structures.
-void Shutdown_init(void);
-void Shutdown_cleanup(void);
-
 // Have main thread wait until trigger is called
 void Shutdown_wait(void);
 
 // Main thread continues once shutdown triggers
 void Shutdown_trigger(void);
-
-// Used for all while loops to see if app is shutting down
-bool Shutdown_isShuttingDown(void);
 
 #endif
