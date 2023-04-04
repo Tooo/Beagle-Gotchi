6ddaab11c9e5e9c4b952fa96e5bb36b9efb5e41c
Gabriel Yeung <gabriel.yeung719@gmail.com>
2023-04-03 23:25:22 -0700

    Add Sprite work Add Display numbers for stats TODO go back to menu

--- highLowGame.c
+++ highLowGame.c
@@ -9,6 +9,41 @@
 #include "joystick.h"
 #include "digitDisplay.h"
 #include "pet.h"
+#include "ledMatrix.h"
+
+// Images 
+
+int upArrow [3 * 4] = {
+    8,1,8,
+    1,1,1,
+    8,1,8,
+    8,1,0,
+};
+
+int downArrow [3 * 4] = {
+    8,1,8,
+    8,1,8,
+    1,1,1,
+    8,1,0,
+};
+
+int orWord [8 * 3] = {
+    1,1,1,8,8,1,1,1,
+    1,8,1,8,8,1,8,8,
+    1,1,1,8,8,1,8,8,
+};
+
+int moodWord [19 * 3] = {
+    1,1,1,8,1,1,1,8,1,1,1,8,1,1,8,8,1,1,1,
+    1,1,1,8,1,8,1,8,1,8,1,8,1,8,1,8,8,8,8,
+    1,8,1,8,1,1,1,8,1,1,1,8,1,1,8,8,1,1,1,
+};
+
+int frenWord [19 * 3] = {
+    1,1,1,8,1,1,1,8,1,1,1,8,1,1,8,8,1,1,1,
+    1,1,8,8,1,8,8,8,1,1,8,8,1,8,1,8,8,8,8,
+    1,8,8,8,1,8,8,8,1,1,1,8,1,8,1,8,1,1,1,
+};
 
 // Game Thread
 static void *hlGameThreadFunction(void *arg);
@@ -44,32 +79,55 @@ void *hlGameThreadFunction(void *arg)
 
     sleepForMs(500);
     long long initTime = getTimeInMs();
-    long long endTime = initTime + (5000);
+    long long endTime = initTime + (6000);
     DigitDisplay_setDigit(initNumber);
     JoystickDirection input;
+    //Draw things on the LED display
+    ledMatrix_drawImage(upArrow, 3, 4, 9, 11);
+    ledMatrix_drawImage(orWord, 8, 3, 12, 11);
+    ledMatrix_drawImage(downArrow, 3, 4, 20, 11);
+    ledMatrix_drawHLine(GREEN, 13, 7, 6);
 
+    char buffer[550];
+    ledMatrix_toString(buffer, true);
+    ledMatrix_toString(buffer, true);
     // Only checks for up and down
-    // TODO: Add a visual timer could be just 5 dots
+    int timerPosition = 0;
     while (getTimeInMs() <= endTime)
     {
         input = Joystick_getDirection();
         if (input = JOYSTICK_UP || JOYSTICK_DOWN) {
             break;
         }
+        ledMatrix_drawHLine(RED, 13, 7, timerPosition);
+        timerPosition = (getTimeInMs() - initTime) / 1000;
     }
+    DigitDisplay_setDigit(0);
+    ledMatrix_fillScreen(BLACK);
 
     // Win
     if ((initNumber < nextNumber && input == JOYSTICK_UP) || (initNumber > nextNumber && input == JOYSTICK_DOWN))
     {
-        Pet_addMood(Pet_getMood() + 200);
-        Pet_addFriendship(Pet_getFriendship + 100);
+        Pet_addMood(200);
+        Pet_addFriendship(100);
+        ledMatrix_drawImage(moodWord, 19, 3, 1, 11);
+        DigitDisplay_setDigit(Pet_getMood());
+        sleepForMs(2000);
 
-        //TODO: Show mood on digit display and write "mood =" to led display do for both 
+        ledMatrix_fillScreen(BLACK);
+        ledMatrix_drawImage(frenWord, 19, 3, 1, 11);
+        DigitDisplay_setDigit(Pet_getFriendship());
+        sleepForMs(2000);
+
+        ledMatrix_fillScreen(BLACK);
     } 
     // Lose
     else 
     {
-        Pet_addMood(Pet_getMood() - 50);
+        Pet_addMood(-50);
+        ledMatrix_drawImage(moodWord, 19, 3, 1, 11);
+        DigitDisplay_setDigit(Pet_getMood());
+        sleepForMs(2000);
     }
 
     //TODO: Somehow signal game is done go back to menu 
